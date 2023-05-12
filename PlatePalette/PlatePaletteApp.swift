710f7ffca70f91379209226c0358882b8bb66d61
Hicret AY <hiicretay@hotmail.com>
2023-05-12 17:00:04 +0300

    Initial Commit

---
+++ PlatePalette/PlatePaletteApp.swift
@@ -0,0 +1,20 @@
+//
+//  PlatePaletteApp.swift
+//  PlatePalette
+//
+//  Created by Hicret Ay on 5/12/23.
+//
+
+import SwiftUI
+
+@main
+struct PlatePaletteApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
